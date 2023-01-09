c490aa8854589e51bac77b94637644ebd1e2fdd7
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2023-01-09 12:09:52 -0500

    add sketch and reset function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let startingSize = 16;
 let sizeInput = document.getElementById("sizeInput");
+let resetButton = document.getElementById("reset");
 
 function populateSketchBox(size) {
   let sketchBox = document.querySelector(".sketchbox");
@@ -10,13 +11,30 @@ function populateSketchBox(size) {
   for (let i = 0; i < totalSquares; i++) {
     let square = document.createElement("div");
     square.style.backgroundColor = "gray";
+    square.classList.add("square");
     sketchBox.append(square);
   }
+  colorTheSquares();
 }
 
-populateSketchBox(startingSize);
+function colorTheSquares() {
+  let squares = document.querySelectorAll(".square");
+  Array.from(squares).forEach((square) => {
+    square.addEventListener("mouseover", function (e) {
+      square.style.backgroundColor = "red";
+    });
+  });
+}
 
 sizeInput.addEventListener("change", () => {
-  console.log("testing");
   populateSketchBox(sizeInput.value);
 });
+
+resetButton.addEventListener("click", () => {
+  let squares = document.querySelectorAll(".square");
+  Array.from(squares).forEach((square) => {
+    square.style.backgroundColor = "gray";
+  });
+});
+
+populateSketchBox(startingSize);
