6d5dffb7ef7329fa0c1f1f8f43c21017a45add8b
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2023-01-09 11:01:51 -0500

    add grid to sketchbox and size options

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+let startingSize = 16;
+let sizeInput = document.getElementById("sizeInput");
 
+function populateSketchBox(size) {
+  let sketchBox = document.querySelector(".sketchbox");
+  sketchBox.innerHTML = "";
+  sketchBox.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  sketchBox.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  let totalSquares = size * size;
+  for (let i = 0; i < totalSquares; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "gray";
+    sketchBox.append(square);
+  }
+}
+
+populateSketchBox(startingSize);
+
+sizeInput.addEventListener("change", () => {
+  console.log("testing");
+  populateSketchBox(sizeInput.value);
+});
